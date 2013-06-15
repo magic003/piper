d84c64440f64a484645e624dc2a937b1ac502e99
Minjie Zha <minjiezha@gmail.com>
2013-06-15 09:35:26 +0800

    add extra to env

--- lib/piper/env.rb
+++ lib/piper/env.rb
@@ -1,7 +1,7 @@
 module Piper
-  Env = Struct.new(:request, :response, :credentials) do
-    def initialize(request=nil, response=nil, credentials={}) 
-      super(request, response, credentials)
+  Env = Struct.new(:request, :response, :credentials, :extra) do
+    def initialize(request=nil, response=nil, credentials={}, extra={}) 
+      super(request, response, credentials, extra)
       yield self if block_given?
     end
   end
